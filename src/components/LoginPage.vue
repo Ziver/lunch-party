6ae7b03c386fc78e255d5153b74eda0b3e4d9508
Ziver <Ziver@users.noreply.github.com>
2022-10-27 11:11:37 +0200

    Added login page content

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,8 +1,22 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
   <h1>Lunch Party!!</h1>
   <h3>Login Page</h3>
+
+  <div>
+    <label for="group">Group Name: </label>
+    <input type="text" id="group" name="group" required />
+  </div>
+  <div>
+    <label for="name">Your User Name: </label>
+    <input type="text" id="name" name="name" required />
+  </div>
+  <div>
+    <button type="button" @click="">Login</button>
+  </div>
 </template>
 
 <style scoped></style>
