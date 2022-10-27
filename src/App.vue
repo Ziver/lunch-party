53219d54e9cb774c56b37bbfaaac61859d0292e3
Ziver <Ziver@users.noreply.github.com>
2022-10-27 16:28:45 +0200

    Added possibility to view and add restaurant

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { ref } from 'vue';
 import LoginPage from './components/LoginPage.vue';
+import VotingPage from './components/VotingPage.vue';
 
 const showLogin = ref(true);
+const showVoting = ref(false);
 
 function loginDone(data) {
   console.log('Main app login done.');
 
   showLogin.value = false;
+  showVoting.value = true;
 }
 </script>
 
@@ -16,6 +19,7 @@ function loginDone(data) {
     <h1>Lunch Party!!</h1>
 
     <LoginPage v-show="showLogin" @response="loginDone" />
+    <VotingPage v-show="showVoting" />
   </div>
 </template>
 
--- src/components/VotingPage.vue
+++ src/components/VotingPage.vue
@@ -1,8 +1,44 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+var restaurantList = ref([
+  {
+    name: 'Tele2 Kista',
+    url: 'https://www.foodandco.se/restauranger/restauranger/kista/',
+  },
+  { name: 'Restaurang 88', url: 'https://restaurang88.se/' },
+  { name: 'Upper East', url: 'https://uppereast.gastrogate.com/lunch/' },
+  {
+    name: 'Tastory',
+    url: 'https://www.tastory.com/sv/vara-restauranger--cafeer/restauranger/tastory-kista/',
+  },
+  { name: 'The Place(World Class)', url: 'https://theplace.kvartersmenyn.se/' },
+  { name: 'Glaze', url: 'https://vasakronan.foodbycoor.se/glaze' },
+  {
+    name: 'Nordic Forum',
+    url: 'https://nordicforum.gastrogate.com/lunchmeny/',
+  },
+  { name: 'Heat', url: 'https://heatrestauranger.se/veckans-lunchmeny/' },
+  { name: 'Wild Kitchen', url: 'https://www.kvartersmenyn.se/rest/16057' },
+]);
+
+function addRestaurant() {
+  var restaurantName = prompt('Enter the name of a restaurant:');
+
+  restaurantList.value.push({ name: restaurantName, url: '' });
+}
+</script>
 
 <template>
   <div>
     <h3>Start Voting!</h3>
+    <button type="button" @click="addRestaurant">+</button>
+
+    <div>
+      <div v-for="restaurant in restaurantList">
+        {{ restaurant.name }} {{ restaurant.url }}
+      </div>
+    </div>
   </div>
 </template>
 
