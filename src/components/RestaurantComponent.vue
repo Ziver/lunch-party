2028750a268a0c7c915c37f3e0c6153fddbf02f9
Ziver <Ziver@users.noreply.github.com>
2022-10-27 17:14:48 +0200

    Nice boxes

--- src/components/RestaurantComponent.vue
+++ src/components/RestaurantComponent.vue
@@ -8,7 +8,18 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>{{ name }} {{ url }}</div>
+  <div class="restaurant-box">{{ name }} {{ url }}</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.restaurant-box {
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  background: #393f47;
+  width: 400px;
+  height: 60px;
+  padding: 10px;
+  border: 2px solid gray;
+  margin: 5px;
+  overflow: hidden;
+}
+</style>
